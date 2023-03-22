6e0f69e2cddd753d5ec9f7647ba07f05a606e4bc
amiram <ah.amiri.1985@gamil.com>
2023-03-22 12:13:02 +0100

    start of project

---
+++ src/main/java/com/am/petshop/user/model/User.java
@@ -0,0 +1,26 @@
+package com.am.petshop.user.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long user_id;
+	@Column(name = "firstname", length = 25, nullable = false)
+	private String firstname;
+	@Column(name = "lastname", length = 25, nullable = false)
+	private String lastname;
+	@Column(name = "username", length = 20, nullable = false)
+	private String username;
+	@Column(name = "email", length = 40, nullable = false)
+	private String email;
+	@Column(name = "password", length = 20, nullable = false)
+	private String password;
+	@Column(name = "phone", length = 12, nullable = false)
+	private String phone;
+
+}
---
+++ src/test/java/com/am/petshop/PetshopApplicationTests.java
@@ -0,0 +1,9 @@
+package com.am.petshop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+
+}
